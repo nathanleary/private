698bef17e65afb4e3371cd04f81dc5cc0393ede1
Nathan Leary <nathan-leary@mail.com>
2022-02-12 00:33:06 +1000

    Update main.go

--- main.go
+++ main.go
@@ -1,37 +1,38 @@
 /* Zaki - 6 April 2018 - Initial creation to demonstrate DLL call in Golang */
-// package main
-
-// import (
-//     "fmt"
-//     "log"
-//     "syscall"
-// )
-
 package main
 
-import "C"
-
-
-//export PrintBye
-func PrintBye() string {
-   return ("From DLL: Bye!")
-}
+import (
+    "fmt"
+    "log"
+    "syscall"
+)
 
 func main() {
-    // Need a main function to make CGO compile package as C shared library
+    h, e := syscall.LoadLibrary("dll.dll")   //Make sure this DLL follows Golang machine bit architecture (64-bit in my case)
+    if e != nil {
+        log.Fatal(e)
+    }
+    defer syscall.FreeLibrary(h)
+    proc, e := syscall.GetProcAddress(h, "PrintBye") //One of the functions in the DLL
+    if e != nil {
+        log.Fatal(e)
+    }
+    
+    n, _, _ := syscall.Syscall9(uintptr(proc), 0, 2, 2, 2, 2, 0, 0, 0, 0, 0)  //Pay attention to the positioning of the parameter
+    fmt.Printf("Hello dll function returns %d\n", n)
 }
 
+////////
+// package main
+
+// import "C"
+
+
+// //export PrintBye
+// func PrintBye() string {
+//    return ("From DLL: Bye!")
+// }
+
 // func main() {
-//     h, e := syscall.LoadLibrary("dll.dll")   //Make sure this DLL follows Golang machine bit architecture (64-bit in my case)
-//     if e != nil {
-//         log.Fatal(e)
-//     }
-//     defer syscall.FreeLibrary(h)
-//     proc, e := syscall.GetProcAddress(h, "PrintBye") //One of the functions in the DLL
-//     if e != nil {
-//         log.Fatal(e)
-//     }
-    
-//     n, _, _ := syscall.Syscall9(uintptr(proc), 0, 2, 2, 2, 2, 0, 0, 0, 0, 0)  //Pay attention to the positioning of the parameter
-//     fmt.Printf("Hello dll function returns %d\n", n)
+//     // Need a main function to make CGO compile package as C shared library
 // }
