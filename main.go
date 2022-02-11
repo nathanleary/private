4a3bb77c724d20f61f5aa419ec00719fd417dcca
Nathan Leary <nathan-leary@mail.com>
2022-02-12 00:29:11 +1000

    Update main.go

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 /* Zaki - 6 April 2018 - Initial creation to demonstrate DLL call in Golang */
+// package main
+
+// import (
+//     "fmt"
+//     "log"
+//     "syscall"
+// )
+
 package main
 
-import (
-    "fmt"
-    "log"
-    "syscall"
-)
+import "C"
+import "fmt"
+
+//export PrintBye
+func PrintBye() {
+    fmt.Println("From DLL: Bye!")
+}
 
 func main() {
-    h, e := syscall.LoadLibrary("dll.dll")   //Make sure this DLL follows Golang machine bit architecture (64-bit in my case)
-    if e != nil {
-        log.Fatal(e)
-    }
-    defer syscall.FreeLibrary(h)
-    proc, e := syscall.GetProcAddress(h, "PrintBye") //One of the functions in the DLL
-    if e != nil {
-        log.Fatal(e)
-    }
+    // Need a main function to make CGO compile package as C shared library
+}
+
+func main() {
+//     h, e := syscall.LoadLibrary("dll.dll")   //Make sure this DLL follows Golang machine bit architecture (64-bit in my case)
+//     if e != nil {
+//         log.Fatal(e)
+//     }
+//     defer syscall.FreeLibrary(h)
+//     proc, e := syscall.GetProcAddress(h, "PrintBye") //One of the functions in the DLL
+//     if e != nil {
+//         log.Fatal(e)
+//     }
     
-    n, _, _ := syscall.Syscall9(uintptr(proc), 0, 2, 2, 2, 2, 0, 0, 0, 0, 0)  //Pay attention to the positioning of the parameter
-    fmt.Printf("Hello dll function returns %d\n", n)
+//     n, _, _ := syscall.Syscall9(uintptr(proc), 0, 2, 2, 2, 2, 0, 0, 0, 0, 0)  //Pay attention to the positioning of the parameter
+//     fmt.Printf("Hello dll function returns %d\n", n)
 }
