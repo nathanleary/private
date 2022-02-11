5f3337386ffe364872a8abba39fac560d2b3a6e5
Nathan Leary <nathan-leary@mail.com>
2022-02-12 00:07:31 +1000

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+/*
+int function(int);
+#define DEFINE_JUMPER(x) \
+        void *_godl_##x = (void*)0; \
+        __asm__(".global "#x"\n\t"#x":\n\tmovq _godl_"#x"(%rip),%rax\n\tjmp *%rax\n")
+DEFINE_JUMPER(function);
+*/
+
+
 import "C"
 
 import (
