ba60a7d397a2c38ccc58926c0e260a56037fdf71
Nathan Leary <nathan-leary@mail.com>
2022-02-11 23:50:42 +1000

    Update main.go

--- main.go
+++ main.go
@@ -16,8 +16,9 @@ var mod = syscall.NewLazyDLL("dll.dll")
 
 var proc = mod.NewProc("PrintBye")
 
-ret, _, _ := proc.Call()
+ptr := proc.Addr()
+r1, r2, err := syscall.Syscall(ptr, 0, 0, 0, 0)
 
-fmt.Printf("Return: %d\n", ret)
+fmt.Printf("Return: ", r1, r2, err)
 
 }
