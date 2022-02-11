117d22127ea94d8b2f6ad6a5bfc8e2a8910778f0
Nathan Leary <nathan-leary@mail.com>
2022-02-11 23:46:10 +1000

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,12 @@
+package main
+
 import "C"
 
-import (
-	"github.com/sudachen/go-dl/dl"
-	"runtime"
-	"unsafe"
-)
-
-func init() {
-    urlbase := "https://github.com/sudachen/go-dl/releases/download/initial/"
-    if runtime.GOOS == "linux" && runtime.GOARCH == "amd64"{
-        so := dl.Load(
-            dl.Cache("plgin.so"),
-            dl.LzmaExternal(urlbase+"libfunction_lin64.lzma"))
-    } else if runtime.GOOS == "windows" && runtime.GOARCH == "amd64" {
-        so := dl.Load(
-            dl.Cache("plugin.dll"),
-            dl.LzmaExternal(urlbase+"libfunction_win64.lzma"))
-    }
-    so.Bind("function",unsafe.Pointer(&C._godl_function))
+//export s
+func s() string {
+	return "HELLO"
 }
 
 func main() {
-    C.function(0)
+	// Need a main function to make CGO compile package as C shared library
 }
